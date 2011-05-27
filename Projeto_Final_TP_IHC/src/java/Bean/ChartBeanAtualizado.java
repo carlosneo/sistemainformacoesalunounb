4e1383cc542b4f09dd1a8dfc45adbe94487112d9
carlos.neo@gmail.com <carlos.neo@gmail.com@10ffc291-88d5-48e6-7ced-d50f9c56b786>
2011-05-27 15:09:33 +0000

    Criacao de mais alguns graficos em paginaProfessor.xhtml e suas respectivas classes em java

---
+++ Projeto_Final_TP_IHC/src/java/Bean/ChartBeanAtualizado.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import java.util.*;
+import javax.faces.bean.ManagedBean;
+@ManagedBean (name="chartBeanAtualizado")
+
+/**
+ *
+ * @author Neo
+ */
+public class ChartBeanAtualizado {
+    
+    private List<Sale> sales;
+
+	public ChartBeanAtualizado() {
+		sales = new ArrayList<Sale>();
+		sales.add(new Sale("Jornais", 120));
+		sales.add(new Sale("Revistas", 200));
+                sales.add(new Sale("TV", 420));
+		sales.add(new Sale("Rádio", 300));
+		sales.add(new Sale("Internet", 500));
+		
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+    
+}
