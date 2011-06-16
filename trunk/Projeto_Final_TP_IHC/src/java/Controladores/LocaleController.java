bf40fa920d1afaf2cb1b19ddf1b55b6400b10067
carlos.neo@gmail.com <carlos.neo@gmail.com@10ffc291-88d5-48e6-7ced-d50f9c56b786>
2011-06-16 18:42:11 +0000

    Primeira tentativa de implementar a internacionalizacao. Criacao das classes controladoras. Internacionalizacao ainda nao funcionando

---
+++ trunk/Projeto_Final_TP_IHC/src/java/Controladores/LocaleController.java
@@ -0,0 +1,30 @@
+package Controladores;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.text.MessageFormat;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+public class LocaleController {
+	private Locale currentLocale = new Locale("pt", "BR");
+
+	public void englishLocale() {
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		currentLocale = Locale.US;
+		viewRoot.setLocale(currentLocale);
+	}
+        
+        private static ResourceBundle myResources =
+	      ResourceBundle.getBundle("Messages", Locale.getDefault());
+
+	public void portugueseLocale() {
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		currentLocale = new Locale("pt", "BR");
+		viewRoot.setLocale(currentLocale);
+	}
+
+	public Locale getCurrentLocale() {
+		return currentLocale;
+	}
+}
