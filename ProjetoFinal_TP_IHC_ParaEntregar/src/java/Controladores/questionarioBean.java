4234753314aabe3044f128af80725f309ac0fb23
gfv.unb@gmail.com <gfv.unb@gmail.com@10ffc291-88d5-48e6-7ced-d50f9c56b786>
2011-06-30 15:27:57 +0000

    Adicionado Questionario.java e QuestionarioBean.java como tentativas de uma validação do questionario

---
+++ ProjetoFinal_TP_IHC_ParaEntregar/src/java/Controladores/questionarioBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Guilherme
+ */
+@ManagedBean (name="questionarioBean")
+@RequestScoped
+public class QuestionarioBean {
+    
+    private Questionario questionario;
+    /** Creates a new instance of QuestionarioBean */
+    public QuestionarioBean() {
+        questionario = new Questionario();
+    }
+    
+ public String ValidadorQuestionario() {
+        if (questionario.getQuestão2Item1().equalsIgnoreCase("") && questionario.getQuestão2Item2().equalsIgnoreCase("")) {
+            
+            return "questao2Item1ACEITO";
+           
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null , new FacesMessage(FacesMessage.SEVERITY_ERROR,"O número da matricula ou a senha estão incorretos. Tente novamente.", "Login invalido!"));
+           
+            return "questao2Item1ERRO";
+        } 
+    }
+    
+    
+}
