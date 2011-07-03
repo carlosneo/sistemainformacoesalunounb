a78bffdbecf082dd44b60a858ff440a9feda2927
carlos.neo@gmail.com <carlos.neo@gmail.com@10ffc291-88d5-48e6-7ced-d50f9c56b786>
2011-07-03 19:07:17 +0000

    Primeiros passos da geracao de logs
    log gerado ao logar aluno e logar professor, tanto quando Ok, quando ocorre erro

--- ProjetoFinal_TP_IHC_ParaEntregar/src/java/Controladores/ChartBeanSexo.java
+++ ProjetoFinal_TP_IHC_ParaEntregar/src/java/Controladores/ChartBeanSexo.java
@@ -4,9 +4,12 @@
  */
 package Controladores;
 
-
+import java.util.logging.Logger;
 import java.util.*;
 import javax.faces.bean.ManagedBean;
+import java.util.ResourceBundle;
+import java.util.Locale;
+import java.text.MessageFormat;
 @ManagedBean (name="chartBeanSexo")
 
 /**
@@ -14,11 +17,14 @@ import javax.faces.bean.ManagedBean;
  * @author Guilherme
  */
 public class ChartBeanSexo  {
+    
+        private static Logger log = Logger.getLogger(ChartBeanSexo.class.getName());
  
 	private List<Sale> sales;
 
 	public ChartBeanSexo() {
 		sales = new ArrayList<Sale>();
+                log.info("Grafico relativo ao sexo dos alunos gerado com sucesso");
 		sales.add(new Sale("Masculino", 540));
 		sales.add(new Sale("Feminino", 325));
 		
--- ProjetoFinal_TP_IHC_ParaEntregar/src/java/Controladores/LoginBean.java
+++ ProjetoFinal_TP_IHC_ParaEntregar/src/java/Controladores/LoginBean.java
@@ -4,6 +4,7 @@
  */
 package Controladores;
 
+import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -17,6 +18,8 @@ import javax.faces.context.FacesContext;
 @RequestScoped
 public class LoginBean {
 
+    private static Logger log = Logger.getLogger(LoginBean.class.getName());
+    
     private Login login;
 
     public Login getLogin() {
@@ -34,18 +37,22 @@ public class LoginBean {
     
     public String login() {
         if (login.getLogin().equalsIgnoreCase("1045547") && login.getSenha().equals("1234")){
-           return "SUCESSOaluno" ;
+           log.info("Login Aluno efetuado com sucesso");
+            return "SUCESSOaluno" ;
         } else {
             FacesContext.getCurrentInstance().addMessage(null , new FacesMessage(FacesMessage.SEVERITY_ERROR,"#{msg.promptloginalunomenu7}", "#{msg.promptloginalunomenu8}"));
+            log.info("Erro ao logar aluno");
             return "ERROaluno";
         } 
     }
     
        public String loginProf() {
         if (login.getLogin().equalsIgnoreCase("1234") && login.getSenha().equals("1234")){
-           return "SUCESSOprof" ;
+            log.info("Login Aluno efetuado com sucesso");
+            return "SUCESSOprof" ;
         } else {
             FacesContext.getCurrentInstance().addMessage(null , new FacesMessage(FacesMessage.SEVERITY_ERROR,"O número da matricula ou a senha estão incorretos. Tente novamente.", "Login invalido!"));
+            log.info("Erro ao logar professor");
             return "ERROprof";
         } 
     }
